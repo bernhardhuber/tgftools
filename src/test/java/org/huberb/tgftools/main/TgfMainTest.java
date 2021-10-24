8a13e3f889119a356c840417107d2ff1d80c00f3
Bernhard Huber <bernihuber123@gmail.com>
2021-10-24 23:34:26 +0200

    use objects initialized in setUp() method

--- src/test/java/org/huberb/tgftools/main/TgfMainTest.java
+++ src/test/java/org/huberb/tgftools/main/TgfMainTest.java
@@ -46,14 +46,6 @@ public class TgfMainTest {
     @ParameterizedTest
     @ValueSource(strings = {"--help", "-h"})
     public void testCommandLine_help(String helpOption) {
-        final TgfMain app = new TgfMain();
-        final CommandLine cmd = new CommandLine(app);
-
-        final StringWriter swOut = new StringWriter();
-        cmd.setOut(new PrintWriter(swOut));
-
-        final StringWriter swErr = new StringWriter();
-        cmd.setErr(new PrintWriter(swErr));
         //---
         final int exitCode = cmd.execute(helpOption);
         assertEquals(0, exitCode);
